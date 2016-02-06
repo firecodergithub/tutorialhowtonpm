c331c155a9460c0d9c14eb9430c36295e3c5233b
Vlad <vlad@Vl-MBP.local>
2016-02-07 01:51:12 +0200

    added error handling

--- callmodulefls.js
+++ callmodulefls.js
@@ -1,2 +1,3 @@
 var modfls=require("./modulefls");
-modfls(process.argv[2],process.argv[3],function(er,data){for (i=0;i<data.length;i++) console.log(data[i].toString());});
+modfls(process.argv[2],process.argv[3],function(er,data){if (er) {console.log("There was an error:"+er);return;} 
+						for (i=0;i<data.length;i++) console.log(data[i].toString());});
