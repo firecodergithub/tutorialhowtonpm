65d5a4eb52dd1a78d5621bff71adbc49fb2d1695
Tesseract <tatalor@yahoo.com>
2016-02-06 23:32:25 +0000

    modular ext ls

---
+++ callmodulefls.js
@@ -0,0 +1,2 @@
+var modfls=require("./modulefls");
+modfls(".","js",function(er,data){for (i=0;i<data.length;i++) console.log(data[i].toString());});
---
+++ modulefls.js
@@ -0,0 +1,17 @@
+module.exports=function(fold,ext,callb)
+{
+var path=require('path');
+var fs=require('fs');
+var f=fs.readdir(fold,function(err, data){
+			if (err!=null) callb(err); //call the callback just with the error
+			var ar=[];
+			for (i=0;i<data.length;i++) 
+				{
+					if (path.extname(data[i].toString())=="."+ext)
+						 {
+							ar.push(data[i].toString());
+						}
+				}
+				callb(null,ar); //pass the list to the callback function
+			});
+}
