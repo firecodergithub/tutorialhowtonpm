4872df36b570574d739f4428023b0df04f3dd18f
Tesseract <tatalor@yahoo.com>
2016-02-20 15:01:40 +0000

    revert to text header

--- httpfileserver.js
+++ httpfileserver.js
@@ -4,7 +4,7 @@ var fs=require('fs');
 //create the server using the callback
 var server=http.createServer(function(req,res)
 		{
-			res.writeHead(200, { 'content-type': 'video/mp4' })  ;
+			res.writeHead(200,{'content-type':'text/plain'});
 			var filestream=fs.createReadStream(process.argv[3]);
 			filestream.pipe(res);
 		});
