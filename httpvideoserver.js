9b72b32cdec719b788121159e32944f6a8966643
Tesseract <tatalor@yahoo.com>
2016-02-20 15:16:07 +0000

    initial version

---
+++ httpvideoserver.js
@@ -0,0 +1,11 @@
+var http=require('http');
+var fs=require('fs');
+
+//create the server using the callback
+var server=http.createServer(function(req,res)
+		{
+			res.writeHead(200,{'content-type':'video/mp4'});
+			var filestream=fs.createReadStream(process.argv[3]);
+			filestream.pipe(res);
+		});
+server.listen(process.argv[2]);
