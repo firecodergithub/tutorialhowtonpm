87cfd826a0b3211f700b79b6b8bd17c9a4765d96
Tesseract <tatalor@yahoo.com>
2016-02-20 14:51:47 +0000

    video header mp4

--- httpfileserver.js
+++ httpfileserver.js
@@ -4,7 +4,7 @@ var fs=require('fs');
 //create the server using the callback
 var server=http.createServer(function(req,res)
 		{
-			res.writeHead(200, { 'content-type': 'video/webm' })  ;
+			res.writeHead(200, { 'content-type': 'video/mp4' })  ;
 			var filestream=fs.createReadStream(process.argv[3]);
 			filestream.pipe(res);
 		});
