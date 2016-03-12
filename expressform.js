c0b6e2c18c6b66d69ba7dc34947a4e8edb08b6f6
Vlad <vlad@Vl-MBP.local>
2016-03-12 08:18:56 +0200

    working version

--- expressform.js
+++ expressform.js
@@ -1,12 +1,13 @@
 var express=require('express');
 var path=require('path');
 var app=express();
+app.get('/form',function(req,res) {
+	res.type('html');
+	res.end('<form><input name="str" formmethod="POST" /> </form>');
+});
 var bodyparser=require('body-parser');
 app.use(bodyparser.urlencoded({extended:false}));
-app.get('/form',function(req,res) {
-	res.end('<form><input name="str" /> </form>');
-});
 app.post('/form',function(req,res) {
-	res.body.str.split('').reverse().join('');
+	res.end(req.body.str.split('').reverse().join(''));
 });
 app.listen(process.argv[2]);
